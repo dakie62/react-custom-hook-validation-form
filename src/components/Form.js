4c745e72995bf0ee58baa21c628756aaca94e361
dakie62 <aebrard62730@gmail.com>
2022-08-19 15:23:47 +0200

    feat: deriving state of inputs to check validity of overall form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,47 @@
-import { useRef } from "react";
+import { useState } from "react";
 import styles from "./Form.module.css";
 
+const validateInput = (str) => str.trim().length === 0;
+
 const Form = () => {
-  const nameInput = useRef();
-  const firstNameInput = useRef();
+  const [nameInputState, setNameInput] = useState("");
+  const [firstNameInputState, setFirstNameInput] = useState("");
+
+  // deriving state
+  const isErrorName = validateInput(nameInputState);
+  const isErrorFirstName = validateInput(firstNameInputState);
+  console.log(isErrorName);
+  console.log(isErrorFirstName);
 
   const onSubmitForm = (event) => {
     event.preventDefault();
-    console.log(nameInput.current.value, firstNameInput.current.value);
-    alert("Submitting my form");
+    if (isErrorName || isErrorFirstName) {
+      alert("Formulaire invalide");
+    }
+    console.log("Mon formulaire est valide");
+    console.log("Sending", nameInputState, firstNameInputState);
+  };
+
+  const onChangeNameInputHandler = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const onChangeFirstNameInputHandler = (event) => {
+    setFirstNameInput(event.target.value);
   };
 
   return (
     <form className={styles["form-group"]} onSubmit={onSubmitForm}>
       <div className={styles["form-control"]}>
         <label htmlFor="Nom">Nom</label>
-        <input id="Nom" ref={nameInput}></input>
+        <input id="Nom" onChange={onChangeNameInputHandler}></input>
       </div>
+
       <div className={styles["form-control"]}>
         <label htmlFor="Prenom">Prenom</label>
-        <input id="Prenom" ref={firstNameInput}></input>
+        <input id="Prenom" onChange={onChangeFirstNameInputHandler}></input>
       </div>
+
       <div className={styles["form-action"]}>
         <button>Validate</button>
       </div>
